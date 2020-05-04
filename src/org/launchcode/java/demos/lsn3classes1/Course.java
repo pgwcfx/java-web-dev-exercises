7769dad9328874d46c029cd4042a28ea0fd821a6
Paul Williams <pgwilliams107@gmail.com>
2020-05-04 18:51:40 -0500

    Commit for chapter 5 exercises

--- src/org/launchcode/java/demos/lsn3classes1/Course.java
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -1,45 +1,88 @@
 package org.launchcode.java.demos.lsn3classes1;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Course {
 
     private String subject;
+
     private Double lengthInHours;
-    private ArrayList<String> homework = new ArrayList<>();
-    private ArrayList<String> schoolSupplies = new ArrayList<>();
-    public Course(String subject, Double lengthInHours, ArrayList<String> homework, ArrayList<String> schoolSupplies){
+    private ArrayList<String> homework;
+
+    private ArrayList<String> schoolSupplies;
+
+    public Course(String subject, Double lengthInHours, ArrayList<String> homework, ArrayList<String> schoolSupplies) {
         this.subject = subject;
         this.lengthInHours = lengthInHours;
         this.homework = homework;
         this.schoolSupplies = schoolSupplies;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == this){
+            return true;
+        }
+        if (o == null){
+            return false;
+        }
+        if (o.getClass() != getClass()){
+            return false;
+        }
+        Course course = (Course) o;
+        return subject.equals(course.subject) &&
+                homework.equals(course.homework);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(subject, homework);
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "subject='" + subject + '\'' +
+                ", lengthInHours=" + lengthInHours +
+                ", homework=" + homework +
+                ", schoolSupplies=" + schoolSupplies +
+                '}';
+    }
+
     public String getSubject() {
         return subject;
     }
-    public void setSubject(String aSubject){
-        subject=aSubject;
+
+    public void setSubject(String subject) {
+        this.subject = subject;
     }
 
     public Double getLengthInHours() {
         return lengthInHours;
     }
-    public void setLengthInHours(Double aLengthInHours) {
-        lengthInHours = aLengthInHours;
+
+    public void setLengthInHours(Double lengthInHours) {
+        this.lengthInHours = lengthInHours;
     }
 
     public ArrayList<String> getHomework() {
         return homework;
     }
-    public void setHomework(ArrayList<String> aHomework) {
-        homework = aHomework;
+
+    public void setHomework(ArrayList<String> homework) {
+        this.homework = homework;
     }
 
     public ArrayList<String> getSchoolSupplies() {
         return schoolSupplies;
     }
-    public void setSchoolSupplies(ArrayList<String> aSchoolSupply) {
-        schoolSupplies = aSchoolSupply;
+
+    public void setSchoolSupplies(ArrayList<String> schoolSupplies) {
+
+        this.schoolSupplies = schoolSupplies;
     }
+
 }
+
+
--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -5,10 +5,88 @@ package org.launchcode.java.demos.lsn3classes1;
 
 public class Student {
 
+    private static int nextStudentId = 1;
     private String name;
     private int studentId;
     private int numberOfCredits = 0;
     private double gpa = 0.0;
+    public String gradeLevel;
+    public Student(String name, int studentId){
+        this(name,studentId,0,0);
+    }
+
+    public Student(String name){
+        this(name,nextStudentId);
+        nextStudentId++;
+    }
+
+    public String studentInfo() {
+        return (this.name + " has a GPA of: " + this.gpa);
+    }
+
+    public void addGrade(int courseCredits, double grade){
+        double totalQualityScore = gpa*numberOfCredits;
+        if (grade >= 93 && grade <= 100){
+            totalQualityScore=(4.0*courseCredits)+totalQualityScore;
+            numberOfCredits+=courseCredits;
+            gpa=totalQualityScore/numberOfCredits;
+        } else if (grade < 93 && grade >= 90){
+            totalQualityScore=(3.7*courseCredits)+totalQualityScore;
+            numberOfCredits+=courseCredits;
+            gpa=totalQualityScore/numberOfCredits;
+        } else if (grade < 90 && grade >= 87){
+            totalQualityScore=(3.3*courseCredits)+totalQualityScore;
+            numberOfCredits+=courseCredits;
+            gpa=totalQualityScore/numberOfCredits;
+        } else if (grade < 87 && grade >= 83){
+            totalQualityScore=(3.0*courseCredits)+totalQualityScore;
+            numberOfCredits+=courseCredits;
+            gpa=totalQualityScore/numberOfCredits;
+        } else if (grade < 83 && grade >= 80){
+            totalQualityScore=(2.7*courseCredits)+totalQualityScore;
+            numberOfCredits+=courseCredits;
+            gpa=totalQualityScore/numberOfCredits;
+        } else if (grade < 80 && grade >= 77){
+            totalQualityScore=(2.3*courseCredits)+totalQualityScore;
+            numberOfCredits+=courseCredits;
+            gpa=totalQualityScore/numberOfCredits;
+        } else if (grade < 77 && grade >= 73){
+            totalQualityScore=(2.0*courseCredits)+totalQualityScore;
+            numberOfCredits+=courseCredits;
+            gpa=totalQualityScore/numberOfCredits;
+        } else if (grade < 73 && grade >= 70){
+            totalQualityScore=(1.7*courseCredits)+totalQualityScore;
+            numberOfCredits+=courseCredits;
+            gpa=totalQualityScore/numberOfCredits;
+        } else if (grade < 70 && grade >= 67){
+            totalQualityScore=(1.3*courseCredits)+totalQualityScore;
+            numberOfCredits+=courseCredits;
+            gpa=totalQualityScore/numberOfCredits;
+        }else if (grade < 67 && grade >= 65){
+            totalQualityScore=(1.0*courseCredits)+totalQualityScore;
+            numberOfCredits+=courseCredits;
+            gpa=totalQualityScore/numberOfCredits;
+        } else if (grade < 65){
+            totalQualityScore=(0.0*courseCredits)+totalQualityScore;
+            numberOfCredits+=courseCredits;
+            gpa=totalQualityScore/numberOfCredits;
+        }
+        // Update the appropriate fields: numberOfCredits, gpa
+    }
+
+    public String getGradeLevel() {
+        if (numberOfCredits <= 29 && numberOfCredits >= 0){
+            gradeLevel = "Freshman";
+        } else if (numberOfCredits >= 30 && numberOfCredits <= 59){
+            gradeLevel = "Sophomore";
+        } else if (numberOfCredits >= 60 && numberOfCredits <= 89){
+            gradeLevel = "Junior";
+        } else if (numberOfCredits >= 90){
+            gradeLevel = "Senior";
+        }
+        return gradeLevel;
+        // Determine the grade level of the student based on numberOfCredits
+    }
 
     public Student(String name, int studentId, int numberOfCredits, double gpa){
         this.name=name;
@@ -44,4 +122,27 @@ public class Student {
     public void setGpa(double aGpa) {
         gpa=aGpa;
     }
+
+    public  boolean equals(Object toBeCompared){
+
+        if (toBeCompared == this){
+            return true;
+        }
+
+        if (toBeCompared == null){
+            return false;
+        }
+
+        if (toBeCompared.getClass() != getClass()){
+            return false;
+        }
+
+        Student theStudent = (Student) toBeCompared;
+        return theStudent.getStudentId() == getStudentId();
+    }
+
+    public String toString(){
+        return name + " (Credits: " + numberOfCredits + ", GPA: " + gpa + ")";
+    }
+
 }
